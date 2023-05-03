b883aa4335ebcbeadaba09ecbc7bf928e1cf130f
Robertwlt03 <robertwalter2003@gmail.com>
2023-05-03 23:41:00 +0200

    simple design

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a5f7aef957.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="color.css">
+    <link rel="stylesheet" href="index.css">
+    <script type="text/javascript" src="../Javascript/darkmode.js" defer></script>
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="" class="logo"> <i class="fa-solid fa-house"></i> </a>
+
+            <div class="list-of-links">
+                <a class="links pointer">Test 1</a>
+                <a class="links pointer">Test 2</a>
+                <a class="links pointer">Test 3</a>
+                <a class="links pointer">Test 4</a>
+            </div>
+
+            <div class="settings">
+                <div class="links pointer" id="dark_mode_btn">
+                  <i class="fa-regular fa-moon shadow"></i> Dark Mode off
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
