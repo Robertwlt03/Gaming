2e1749bb36cdecedc1930f3bf39a0a567fbcaa6e
Robertwlt03 <robertwalter2003@gmail.com>
2023-05-25 01:07:20 +0200

    Darkmode & Navbar Update

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 // -------------------------------------------- Exit Navbar --------------------------------------------
-const navbar = document.querySelector(".nav");
+const navDropdown = document.querySelector(".link-background");
+const navButton = document.querySelector(".nav-button");
 
-function unhover (){
-  
+function exitNavbar (){
+    if (navDropdown.style.top === "-100vh") {
+        navDropdown.style.top = "0";
+    } else {
+        navDropdown.style.top = "-100vh"; 
+    }
+
+    if (navDropdown.style.top === "-100vh") {
+        navButton.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    } else {
+        navButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    }
 }
 
-navbar.addEventListener("click", unhover);
-
-
+navButton.addEventListener("click", exitNavbar);
 
 
 
 // -------------------------------------------- Darkmode --------------------------------------------
-const darkmode_button = document.querySelector(".background-switch-darkmode");
-const darkmode_circle = document.querySelector(".switch-circle");
+const darkmodeButton = document.querySelector(".background-switch-darkmode");
+const darkmodeCircle = document.querySelector(".switch-circle");
 
-function switchToggle (){
-  
+function switchDarkmode (){
+   document.body.classList.toggle("darkmode-mode");
+
+   if (document.body.classList.contains("darkmode-mode")) {
+        darkmodeCircle.innerHTML = '<i class="fa-solid fa-moon shadow"></i>';
+        darkmodeCircle.style.animation = "slide-to-right-click 0.3s forwards";
+    } else {
+        darkmodeCircle.innerHTML = '<i class="fa-regular fa-moon shadow"></i>';
+        darkmodeCircle.style.animation = "slide-to-left-click 0.3s forwards";
+    }
+
 }
+
+darkmodeButton.addEventListener("click", switchDarkmode);
 
